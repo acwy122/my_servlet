6de54338212792863b19b046f0f6c30227620fb9
xurb <xurongbiao@hmtim.com>
2022-05-08 19:48:47 +0800

    dierci

--- src/com/mashibing/controller/MyServlet.java
+++ src/com/mashibing/controller/MyServlet.java
@@ -13,7 +13,6 @@ public class MyServlet extends HttpServlet {
         System.out.println(this.getClass().getName());
         String username = req.getParameter("username");
         req.getSession().setAttribute("username",username);
-
         req.getRequestDispatcher("index.jsp").forward(req,resp);
     }
 
